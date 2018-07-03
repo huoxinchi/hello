ed97375128cff9a5782fe2bc3156de40b30d1768
huoxinchi <huoxinchi@outlook.com>
2018-07-03 17:51:15 +0800

    提交文件

---
+++ app/src/main/java/com/example/huoxinchi/test1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.huoxinchi.test1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    int anInt = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String s = null;
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+}
